c9f76851fd355bfbe0b441a2ceb9dfb0065d8af2
Alfredo Miranda  Flores <alfredo.miranda.learning@gmail.com>
2023-05-03 19:07:44 -0600

    finish to add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,21 +30,68 @@ dependencies {
 	implementation("org.springframework.data:spring-data-relational")
 	implementation("org.springframework.boot:spring-boot-autoconfigure")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.liquibase:liquibase-core:4.20.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.data:spring-data-jdbc")
-	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-	implementation("org.yaml:snakeyaml:2.0")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("mysql:mysql-connector-java:8.0.32")
-	runtimeOnly("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	implementation("org.testcontainers:mysql:1.17.6")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	// Manage versions of all kotlin components
+	implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+	// Use kotlin JDK 8 standard library
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${property("kotlinxCoroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:${property("kotlinxCoroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${property("kotlinxCoroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${property("kotlinxCoroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${property("kotlinxCoroutinesVersion")}")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("com.fasterxml.jackson.core:jackson-databind:${property("jacksonDatabindVersion")}")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.github.microutils:kotlin-logging-jvm:${property("kotlinLoggingJvm")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j")
+
+	implementation("org.liquibase:liquibase-core:${property("liquibaseCoreVersion")}")
+
+	implementation("org.mapstruct:mapstruct:${property("mapstructVersion")}")
+	implementation("org.mapstruct:mapstruct-processor:${property("mapstructVersion")}")
+
+	implementation("io.github.resilience4j:resilience4j-circuitbreaker:${property("resilience4jCircuitbreaker")}")
+	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j:${property("reactorResilience4jVersion")}")
+	implementation("io.github.resilience4j:resilience4j-kotlin:${property("resilience4jKotlin")}")
+
+	implementation("io.swagger.core.v3:swagger-annotations:${property("swaggerAnnotations")}")
+	implementation("org.springdoc:springdoc-openapi-ui:${property("openapiVersion")}")
+	implementation("org.springdoc:springdoc-openapi-webflux-ui:${property("openapiVersion")}")
+	implementation("org.springdoc:springdoc-openapi-kotlin:${property("openapiVersion")}")
+
+	implementation("org.yaml:snakeyaml:${property("snakeYamlVersion")}")
+
+	runtimeOnly("com.mysql:mysql-connector-j:${property("mysqlConnectorVersion")}")
+	runtimeOnly("dev.miku:r2dbc-mysql:${property("r2dbcMysqlVersion")}")
+
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.0.6")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:mysql:${property("testContainersVersion")}")
+	testImplementation("org.testcontainers:junit-jupiter:${property("testContainersVersion")}")
+	testImplementation("org.testcontainers:kafka:${property("testContainersVersion")}")
+	testImplementation("org.testcontainers:testcontainers:${property("testContainersVersion")}")
+	testImplementation("org.testcontainers:mockserver:${property("testContainersVersion")}")
+	testImplementation("io.mockk:mockk:${property("mockkVersion")}")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testImplementation("org.mock-server:mockserver-client-java:${property("mockserver")}") {
+		exclude(group = "javax.servlet", module = "javax.servlet-api")
+	}
+
+	testImplementation("com.ninja-squad:springmockk:${property("springmockk")}")
+	testImplementation("io.github.serpro69:kotlin-faker:${property("kotlinFaker")}")
+	testImplementation("org.awaitility:awaitility:${property("awaitilityVersion")}")
+	testImplementation("org.assertj:assertj-core:${property("assertJVersion")}")
+	testImplementation("com.tngtech.archunit:archunit-junit5:${property("archunit")}")
 }
 
 tasks.withType<KotlinCompile> {
